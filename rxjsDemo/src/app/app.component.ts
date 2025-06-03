5fff094fdde2f04d71eb796a38cae38f75419237
Yogita <radheyavrushali2@gmail.com>
2025-06-03 21:21:57 +0530

    rxjs - Subjects

--- rxjsDemo/src/app/app.component.ts
+++ rxjsDemo/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,27 +11,43 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'rxjsDemo';
   // observable is pre-defined in RxJs.
-  // a multiple asynchronous events will be their 
-  // where one observable go to multiple asynchronous event 
+  // a multiple asynchronous events will be their
+  // where one observable go to multiple asynchronous event
   // one after another by subscribing it
   //httpClient : using it you directly get values
-  obs !:any;
-  msg !:any;
-  constructor(private hc :HttpClient){
+  obs!: any;
+  msg!: any;
+
+  // ---------------------------------Subject----------------------------------
+  subj = new Subject();
+
+  constructor(private hc: HttpClient) {
     this.obs = new Observable((sub) => {
-      // every observable have an observer 
+      // every observable have an observer
       // and will have events like next,error,complete
-    sub.next(1);
-    sub.next(10);
-    sub.error("My Error")
-    sub.next(100);
-    sub.next(1000);
-    sub.complete();
-  });
-   
-  this.msg=this.hc.get('https://official-joke-api.appspot.com/jokes/random');
+      sub.next(1);
+      sub.next(10);
+      sub.error('My Error');
+      sub.next(100);
+      sub.next(1000);
+      sub.complete();
+    });
+   this.msg = this.hc.get(
+      'https://official-joke-api.appspot.com/jokes/random'
+    );
+    // this.obs.subscribe((val: any) => console.log(val));
+    this.subj.subscribe({
+      next: (val) => console.log(val),
+      error: () => console.log('Error'),
+      complete: () => console.log('Completed'),
+    });
+
+ 
   }
   ngOnInit() {
-    this.obs.subscribe((val: any) => console.log(val));
+   this.obs.subscribe((val: any) => console.log(val));
+   this.subj.next(100);
+   this.subj.next(1000);
+   this.subj.complete();
   }
 }
