8130cf292e4a5ec087fb61eb86b8020fc9f5191b
Yogita <radheyavrushali2@gmail.com>
2025-06-05 21:13:05 +0530

    plain dashboard

--- statistisDash/src/app/app.module.ts
+++ statistisDash/src/app/app.module.ts
@@ -4,17 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StatisticsComponent } from './statistics/statistics.component';
-import { VisualStatsComponent } from './visual-stats/visual-stats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StatisticsComponent,
-    VisualStatsComponent
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
