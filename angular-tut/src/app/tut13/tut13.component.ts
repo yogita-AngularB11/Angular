e36b132a25a4dd1e1370569f471ed5536f7b0fb4
Yogita <radheyavrushali2@gmail.com>
2025-06-23 11:47:36 +0530

    Template literal get and set method

--- angular-tut/src/app/tut13/tut13.component.ts
+++ angular-tut/src/app/tut13/tut13.component.ts
@@ -4,18 +4,26 @@ import { Component } from '@angular/core';
   selector: 'app-tut13',
   imports: [],
   templateUrl: './tut13.component.html',
-  styleUrl: './tut13.component.css'
+  styleUrl: './tut13.component.css',
 })
 export class Tut13Component {
-name="";
-getName(event:Event){
-  // console.log(event);
-  // console.log(event.target.value); 
-   //event.target is possibly null
+  name = '';
+  displayName="";
+  getName(event: Event) {
+    // console.log(event);
+    // console.log(event.target.value);
+    //event.target is possibly null
+    const val = (event.target as HTMLInputElement).value;
+    console.log(val);
+    this.name=val;
+  }
 
-   const name=(event.target as HTMLInputElement).value
-console.log(name);
+  showName(){
+    const value=this.name;
+    this.displayName=value;
+  }
 
-  
-}
+  setName(){
+    this.name="Sanyukta";
+  }
 }
