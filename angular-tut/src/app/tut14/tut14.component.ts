486c087605b915ce9aa5b745e6e28f4a9f526eda
Yogita <radheyavrushali2@gmail.com>
2025-06-25 17:32:47 +0530

    Update tut14 component logic

--- angular-tut/src/app/tut14/tut14.component.ts
+++ angular-tut/src/app/tut14/tut14.component.ts
@@ -4,13 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-tut14',
   imports: [],
   templateUrl: './tut14.component.html',
-  // styleUrl: './tut14.component.css'
+  styleUrl: './tut14.component.css'
   // styles:['h1{background-color:pink;}']
   // styles:[`h1
   // {
   //   background-color:yellow;
   // }`]  array of css files 
-  styleUrls:['./tut14.component.css','./tut14.btn.css'] 
+  // styleUrls:['./tut14.component.css','./tut14.btn.css'] 
 })
 export class Tut14Component {
 
