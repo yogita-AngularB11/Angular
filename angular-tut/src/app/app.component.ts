87f7d8284ddcf3c59db0707c0f44256222df3953
Yogita <radheyavrushali2@gmail.com>
2025-07-10 11:36:00 +0530

    directives

--- angular-tut/src/app/app.component.ts
+++ angular-tut/src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
+import { Tut27Component } from './tut27/tut27.component';
 
-// import { Tut13Component } from './tut13/tut13.component';
-// import { Tut14Component } from './tut14/tut14.component';
-// import { Tut15Component } from './tut15/tut15.component';
-// import { Tut16Component } from './tut16/tut16.component';
-// import { Tut17Component } from './tut17/tut17.component';
-// import { Tut18Component } from './tut18/tut18.component';
-// import { Tut19Component } from './tut19/tut19.component';
-// import { Tut22Component } from './tut22/tut22.component';
-import { Tut26Component } from './tut26/tut26.component';
 
 @Component({
   selector: 'app-root',
   // imports: [Tut13Component,Tut14Component,Tut15Component,Tut16Component,Tut17Component,Tut18Component,Tut19Component],
-  imports:[Tut26Component],
+  imports:[Tut27Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-tut/src/app/tut27/tut27.component.ts
+++ angular-tut/src/app/tut27/tut27.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-tut27',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './tut27.component.html',
   styleUrl: './tut27.component.css'
 })
 export class Tut27Component {
-
+show=true;
 }
